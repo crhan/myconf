5840112d049dc463e252f90d26ca790a46c10178
Crhan <crhan123@gmail.com>
2011-09-14 23:58:23 +0800

    vim changes

--- vimFiles/generate_helptags.sh
+++ vimFiles/generate_helptags.sh
@@ -3,9 +3,17 @@
 DOC=$( find $1 -type d -name doc )
 CWD=$( cd $( dirname $0 ) && pwd )
 
+GREEN="[32;01m"
+RED="[31;01m"
+PURP="[35;01m"
+BLDWHT="[37;01m"
+BLDYEL="[33;01m"
+OFF="[0m"
+
 for i in $DOC;do
+    i=${i#./}
     cd $i
     vim "+:helptags ." "+:q"
-    [ -f tags ] && echo "$i/tags generated" || "$i/tags generate failed"
+    [ -f tags ] && echo "${GREEN}Generated${OFF} $i/tags" || "${RED}Failed${OFF} $i/tags"
     cd $CWD
 done
